2161c445e8e595dfcd0346800c5446a6b3ff0bd3
Aayush Jha <aayushjha5@gmail.com>
2022-05-17 15:45:26 +0530

    update in UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -64,7 +64,7 @@ class _HomePageState extends State<HomePage> {
         break;
       case "play_channel":
         final id = response["id"];
-        _audioPlayer.pause();
+        //_audioPlayer.pause();
         MyRadio newRadio = radios!.firstWhere((element) => element.id == id);
         radios!.remove(newRadio);
         radios!.insert(0, newRadio);
@@ -113,12 +113,14 @@ class _HomePageState extends State<HomePage> {
     radios = MyRadioList.fromJson(radioJson).radios;
     _selectedRadio = radios![0];
     _selectedColor = Color(int.parse(_selectedRadio.color));
+    print(radios);
     setState(() {});
   }
 
   _playMusic(String url) {
     _audioPlayer.play(url);
     _selectedRadio = radios!.firstWhere((element) => element.url == url);
+    print(_selectedRadio.name);
     setState(() {});
   }
 
@@ -193,74 +195,83 @@ class _HomePageState extends State<HomePage> {
                 );
               },
             )
-          ].vStack(),
+          ].vStack(alignment: MainAxisAlignment.start),
           30.heightBox,
-          VxSwiper.builder(
-            itemCount: radios!.length,
-            aspectRatio: 1.0,
-            enlargeCenterPage: true,
-            onPageChanged: (index) {
-              _selectedRadio = radios![index];
-              setState(() {});
-            },
-            itemBuilder: (context, index) {
-              final rad = radios![index];
+          radios != null
+              ? VxSwiper.builder(
+                  itemCount: radios!.length,
+                  aspectRatio: 1.0,
+                  enlargeCenterPage: true,
+                  onPageChanged: (index) {
+                    _selectedRadio = radios![index];
+                    final colorHex = radios![index].color;
+                    _selectedColor = Color(int.parse(colorHex));
+                    setState(() {});
+                  },
+                  itemBuilder: (context, index) {
+                    final rad = radios![index];
 
-              return VxBox(
-                      child: ZStack(
-                [
-                  Positioned(
-                    top: 0.0,
-                    right: 0.0,
-                    child: VxBox(
-                            child:
-                                rad.category.text.uppercase.white.make().px16())
-                        .height(40)
-                        .black
-                        .alignCenter
-                        .withRounded(value: 10.0)
-                        .make(),
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: VStack(
+                    return VxBox(
+                            child: ZStack(
                       [
-                        rad.name.text.xl3.white.bold.make(),
-                        5.heightBox,
-                        rad.tagline.text.sm.white.semiBold.make(),
-                      ],
-                      crossAlignment: CrossAxisAlignment.center,
-                    ),
-                  ),
-                  Align(
-                      alignment: Alignment.center,
-                      child: [
-                        Icon(
-                          CupertinoIcons.play_circle,
-                          color: Colors.white,
+                        Positioned(
+                          top: 0.0,
+                          right: 0.0,
+                          child: VxBox(
+                                  child: rad.category.text.uppercase.white
+                                      .make()
+                                      .px16())
+                              .height(40)
+                              .black
+                              .alignCenter
+                              .withRounded(value: 10.0)
+                              .make(),
                         ),
-                        10.heightBox,
-                        "Double tap to play".text.gray300.make(),
-                      ].vStack())
-                ],
-              ))
-                  .clip(Clip.antiAlias)
-                  .bgImage(
-                    DecorationImage(
-                      image: NetworkImage(rad.image),
-                      fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(
-                          Colors.black.withOpacity(0.3), BlendMode.darken),
-                    ),
-                  )
-                  .withRounded(value: 60.0)
-                  .border(color: Colors.black, width: 5.0)
-                  .make()
-                  .onInkDoubleTap(() {
-                _playMusic(rad.url);
-              }).p16();
-            },
-          ).centered(),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: VStack(
+                            [
+                              rad.name.text.xl3.white.bold.make(),
+                              5.heightBox,
+                              rad.tagline.text.sm.white.semiBold.make(),
+                            ],
+                            crossAlignment: CrossAxisAlignment.center,
+                          ),
+                        ),
+                        Align(
+                            alignment: Alignment.center,
+                            child: [
+                              Icon(
+                                CupertinoIcons.play_circle,
+                                color: Colors.white,
+                              ),
+                              10.heightBox,
+                              "Double tap to play".text.gray300.make(),
+                            ].vStack())
+                      ],
+                    ))
+                        .clip(Clip.antiAlias)
+                        .bgImage(
+                          DecorationImage(
+                            image: NetworkImage(rad.image),
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(
+                                Colors.black.withOpacity(0.3),
+                                BlendMode.darken),
+                          ),
+                        )
+                        .withRounded(value: 60.0)
+                        .border(color: Colors.black, width: 5.0)
+                        .make()
+                        .onInkDoubleTap(() {
+                      _playMusic(rad.url);
+                    }).p16();
+                  },
+                ).centered()
+              : Center(
+                  child:
+                      CircularProgressIndicator(backgroundColor: Colors.white),
+                ),
           Align(
             alignment: Alignment.bottomCenter,
             child: [
